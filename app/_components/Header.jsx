d0ed2be9311dfd4a8e06f86efba976be6f1014d1
Humira Alam <humiraalam.uiu@gmail.com>
2025-02-03 17:07:56 +0600

    sign up authentication developed

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,5 +1,6 @@
-'use client';
+"use client";
 import { Button } from "@/components/ui/button";
+import { UserButton, useUser } from "@clerk/nextjs";
 import { LogIn, Plus } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,18 +9,22 @@ import React, { useEffect } from "react";
 
 export default function Header() {
   const path = usePathname();
+  const { user, isSignedIn } = useUser();
   useEffect(() => {
-    console.log(path)
+    console.log(path);
   }, []);
-  
-  
+
   return (
-    <div className="p-6 px-10 flex justify-between shadow-md shadow-secondary fixed top-0 w-full z-10 " >
+    <div className="p-6 px-10 flex justify-between shadow-md shadow-secondary fixed top-0 w-full z-10 ">
       <div className="flex gap-12 items-center">
         <Image src={"/logo.svg"} alt="logo" width={150} height={150} />
         <ul className="hidden md:flex gap-10">
           <Link href="/">
-            <li className={`"hover:text-secondary font-medium cursor-pointer" ${path === "/" ? "text-secondary" : ""}`}>
+            <li
+              className={`"hover:text-secondary font-medium cursor-pointer" ${
+                path === "/" ? "text-secondary" : ""
+              }`}
+            >
               For Sale
             </li>
           </Link>
@@ -31,14 +36,19 @@ export default function Header() {
           </li>
         </ul>
       </div>
-      <div className="flex gap-2">
+      <div className="flex gap-2 items-center">
         <Button className="flex gap-2">
           <Plus />
           Post Ad Here
         </Button>
-        <Button variant="outline">
-          <LogIn /> Sign In
-        </Button>
+
+        {isSignedIn ? (
+          <UserButton />
+        ) : (
+          <Button variant="outline">
+            <LogIn /> Sign In
+          </Button>
+        )}
       </div>
     </div>
   );
